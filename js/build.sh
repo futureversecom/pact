fbf2ab3db0eb2086c8ad89a568a5c8849f0f05ea
zees-dev <dev.786zshan@gmail.com>
2024-02-14 10:56:19 +1300

    build script updates, js package updates

--- js/build.sh
+++ js/build.sh
@@ -1,14 +1,31 @@
-#!/bin/bash
-set -ex
-echo "building js pkg for $1 out to: $2"
-wasm-pack build \
-    --target $1 \
-    --scope trn \
-    --out-name pact \
-    --out-dir $2 \
-    --release
+#!/usr/bin/env bash
 
-# Remove wasm-pack generated files
-# They are unintentionally excluding required files when `npm pack` is run
-cd $2
-rm package.json README.md .gitignore LICENSE
+set -ex
+
+# Check if jq is installed
+if ! [ -x "$(command -v jq)" ]; then
+    echo "jq is not installed" >& 2
+    exit 1
+fi
+
+# Clean previous packages
+if [ -d "pkg" ]; then
+    rm -rf pkg
+fi
+
+if [ -d "pkg-node" ]; then
+    rm -rf pkg-node
+fi
+
+PKG_NAME="trn-pact"
+
+# Build for both targets
+rustup run nightly wasm-pack build --target web --scope therootnetwork --out-name $PKG_NAME --release
+rustup run nightly wasm-pack build --target nodejs --scope therootnetwork --out-name $PKG_NAME --release --out-dir pkg-node
+
+# Merge nodejs & browser packages into `pkg/` directory
+cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_main.js"
+sed "s/require[\(]'\.\/${PKG_NAME}/require\('\.\/${PKG_NAME}_main/" "pkg-node/${PKG_NAME}.js" > "pkg/${PKG_NAME}_bg.js"
+jq ".files += [\"${PKG_NAME}_bg.js\"]" pkg/package.json \
+  | jq ".main = \"${PKG_NAME}_main.js\"" > pkg/temp.json
+mv pkg/temp.json pkg/package.json
